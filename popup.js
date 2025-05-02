3730213443df00451800d0f457bd497592fdcb2e
Adnan <macuser@MacUsers-MacBook-Air.local>
2025-05-03 04:19:51 +0600

    adjust some file locations

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-import { getLLMFeedback, getLLMAnswerFromQuestion } from "./Feature Components/llmHandler.js";
+import { getLLMFeedback, getLLMAnswerFromQuestion } from "./llmHandler.js";
 
 console.log("Popup loaded");
 
